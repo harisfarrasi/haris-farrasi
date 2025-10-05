9b687fc23e4a9a5ab544aa968038a3e821219c96
Alwan Haris Farrasi <harisfarrasi@gmail.com>
2025-10-05 10:28:08 +0000

    ubah ini jadi Haris Farrasi (_for element <LinkComponent>_)

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -41,7 +41,7 @@ export function Header() {
       >
         <div className="flex items-center justify-between h-14">
           <Link href="/" className="font-headline text-xl font-bold text-foreground transition-colors hover:text-primary md:px-4">
-            Alwan Haris Farrasi
+            Haris Farrasi
           </Link>
 
           {/* Desktop Navigation */}
