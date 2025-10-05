9263a9b868e4f2d11e218ca1f0f219527ee3e33c
Alwan Haris Farrasi <harisfarrasi@gmail.com>
2025-10-05 08:58:54 +0000

    rapikan ini lagi karena contact ada 2 kali dan ceritaku belum ada (_for

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -11,8 +11,8 @@ export function Header() {
   const headerRef = useRef<HTMLDivElement>(null);
 
   const navLinks = [
+    { href: '#about', label: 'My Story' },
     { href: '#projects', label: 'Projects' },
-    { href: '#contact', label: 'Contact' },
     { href: '#library', label: 'Library' },
   ];
 
