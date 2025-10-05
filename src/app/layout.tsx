bfa8aebb6fe86ba18f5c096a201bf855778f169e
Alwan Haris Farrasi <harisfarrasi@gmail.com>
2025-10-05 06:48:44 +0000

    aku mau font yang sering digunakan oleh angel investor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Poppins:wght@400;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
         <script defer data-domain="yourdomain.com" src="https://plausible.io/js/script.js"></script>
