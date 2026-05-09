f5fc18f001ec5e38115a2ce97935406e8d47779a
Haris Farrasi <macbook@192.168.1.4>
2026-05-09 11:57:21 +0700

    feat: display thesis in LaTeX PDF form and update fonts/UI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ['Inter Tight', 'sans-serif'],
+        headline: ['Inter Tight', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
